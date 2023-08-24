3bde02112533be45f2ba643cfe29119a804ddecf
김재령 <wofud0321@naver.com>
2023-08-24 15:20:27 +0900

    db 연결 및 테스트

---
+++ src/main/java/com/example/JBoard/Repository/UserRepository.java
@@ -0,0 +1,22 @@
+package com.example.JBoard.Repository;
+
+import com.example.JBoard.Entity.User;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserRepository {
+
+    @PersistenceContext
+    EntityManager em;
+
+    public Long save(User user){
+        em.persist((user));
+        return user.getUser_id();
+    }
+
+    public User find(Long user_id){
+        return em.find(User.class, user_id);
+    }
+}
---
+++ src/test/java/com/example/JBoard/Repository/UserRepositoryTest.java
@@ -0,0 +1,35 @@
+package com.example.JBoard.Repository;
+
+import com.example.JBoard.Entity.User;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class UserRepositoryTest {
+
+    @Autowired UserRepository userRepository;
+
+    @Test
+    @Transactional
+    @Rollback(false)
+    public void testUser() {
+        User user = new User();
+        user.setUsername("wofud");
+        Long saveId = userRepository.save(user);
+
+        User findUser = userRepository.find(saveId);
+        System.out.println("findUser = " + findUser);
+
+        assertThat(findUser.getUser_id()).isEqualTo(user.getUser_id());
+        assertThat(findUser.getUsername()).isEqualTo(user.getUsername());
+
+        assertThat(findUser).isEqualTo(user);
+    }
+}
