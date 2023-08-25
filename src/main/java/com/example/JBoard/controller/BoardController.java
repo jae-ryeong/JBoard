b26708420cbd6c33dcd56db0e4e273b16dbf8726
김재령 <wofud0321@naver.com>
2023-08-25 19:48:55 +0900

    컨트롤러 생성

---
+++ src/main/java/com/example/JBoard/controller/BoardController.java
@@ -0,0 +1,18 @@
+package com.example.JBoard.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BoardController {
+
+    @GetMapping("/boardlist")
+    public String boardlist() {
+        return "articles/board";
+    }
+
+    @GetMapping("/boardCreateForm")
+    public String boardCreateForm() {
+        return "articles/boardCreateForm";
+    }
+}
