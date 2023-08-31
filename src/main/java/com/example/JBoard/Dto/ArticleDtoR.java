6f871ccce866c84041a26a3e05ffbcac93d9e090
김재령 <wofud0321@naver.com>
2023-08-31 22:42:14 +0900

    html 수정

--- src/main/java/com/example/JBoard/Dto/ArticleDtoR.java
+++ src/main/java/com/example/JBoard/Dto/ArticleDtoR.java
@@ -5,7 +5,6 @@ import com.example.JBoard.Entity.User;
 public record ArticleDto(
         Long ArticleId,
         String title,
-        User username,  // 이 부분은 확실하지않다 고쳐야 할거같다.
         Integer view_count
 ) {
 }
