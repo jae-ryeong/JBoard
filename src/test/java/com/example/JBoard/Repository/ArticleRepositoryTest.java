c9947371a9383cc9df58137d8241c17b4f7db7da
김재령 <wofud0321@naver.com>
2023-09-03 15:08:04 +0900

    H2 DB로 테스트 실행

--- src/test/java/com/example/JBoard/Repository/ArticleRepositoryTest.java
+++ src/test/java/com/example/JBoard/Repository/ArticleRepositoryTest.java
@@ -11,7 +11,7 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)    // In Memoery 안쓰고 기본 설정된 DB를 사용하겠다.
+//@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)    // In Memoery 안쓰고 기본 설정된 DB를 사용하겠다.
 class ArticleRepositoryTest {   // 레포지토리 테스트는 서비스와 연결X
 
     private final ArticleRepository articleRepository;
@@ -26,33 +26,37 @@ class ArticleRepositoryTest {   // 레포지토리 테스트는 서비스와 연
         List<Article> articles = articleRepository.findAll();
 
         //then
-        assertThat(articles).isNotNull().hasSize(37);
+        assertThat(articles).isNotNull().hasSize(0);
     }
 
     @Test
     public void saveTest() throws Exception{
         //given
-        Article article = Article.of("title", "content", 0L);   // view_count는 자동으로 넣어주었으면 하지만 일단 수동으로 구현
+        Article article = createArticle();   // view_count는 자동으로 넣어주었으면 하지만 일단 수동으로 구현
 
         //when
         Article save = articleRepository.save(article);
         List<Article> articles = articleRepository.findAll();
 
         //then
-        assertThat(articles).isNotNull().hasSize(38);   // test 데이터 37 + 추가한 데이터 1
-        assertThat(save.getArticleId()).isEqualTo(38);
+        assertThat(articles).isNotNull().hasSize(1);   // test 데이터 37 + 추가한 데이터 1
+        assertThat(save.getArticleId()).isEqualTo(1);
     }
 
     @Test
     public void deleteTest() throws Exception{
         //given
-        Article article = Article.of("title", "content", 0L);
+        Article article = createArticle();
 
         //when
         Article save = articleRepository.save(article);
-        articleRepository.deleteById(38L);
+        articleRepository.deleteById(1L);
 
         //then
-        assertThat(articleRepository.findById(38L)).isEmpty();
+        assertThat(articleRepository.findById(1L)).isEmpty();
+    }
+
+    Article createArticle() {
+        return Article.of("title", "content", 0L);
     }
 }
