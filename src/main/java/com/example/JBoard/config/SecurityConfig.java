9d0862efd87205bf055d9d80c67284b1c9c55500
김재령 <wofud0321@naver.com>
2023-09-11 16:20:52 +0900

    게시물 내역까진 접근가능
    상세 페이지 조회 X

---
+++ src/main/java/com/example/JBoard/config/SecurityConfig.java
@@ -0,0 +1,74 @@
+package com.example.JBoard.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.AbstractConfiguredSecurityBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+/*    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {    // login 페이지가 뜨지 않게 해준다.
+        return http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+                .formLogin().and()
+                .build();
+    }*/
+
+/*    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.authorizeHttpRequests(auth -> auth.requestMatchers(new AntPathRequestMatcher("/")).permitAll()).build();
+    }*/
+
+/*    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)  // 스프링부트 3.1부터 이렇게 표현하라고 한다.
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(new AntPathRequestMatcher(PathRequest.toStaticResources().atCommonLocations().toString()))
+                        .permitAll()
+                                .requestMatchers(new AntPathRequestMatcher("/","/articles"))
+                                .permitAll()
+                        .anyRequest().authenticated()   // 위에를 제외한 나머지 request는 인증이 되어야만 한다.
+                )
+                .formLogin().and()
+                .logout()
+                .logoutSuccessUrl("/")
+                .and()
+                .build();
+    }*/
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth ->
+                        auth.requestMatchers(new AntPathRequestMatcher("/detail/**")).authenticated()   // 상세 페이지를 볼 수 없다
+                                //.requestMatchers("/admin/**").access("hasrole('ROLE_ADMIN)'")
+                                .anyRequest().permitAll())
+                .formLogin((formLogin) -> formLogin
+                        .loginPage("/login")
+                        .usernameParameter("userId")
+                        .passwordParameter("password")
+                        .successHandler((request, response, authentication) -> {
+                            System.out.println("authentication : " + authentication.getName());
+                            response.sendRedirect("/boardlist");
+                        })
+                        .failureHandler((request, response, exception) -> {
+                            System.out.println("exception : " + exception.getMessage());
+                            response.sendRedirect("/login");
+                        })
+                        .permitAll()).build();   // 로그인 페이지는 무조건 접근 가능하게.
+
+    }
+
+/*    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {  // 패스워드 암호화
+        return new BCryptPasswordEncoder();
+    }*/
+}
