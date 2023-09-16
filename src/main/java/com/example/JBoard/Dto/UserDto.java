00c738e4ce7da429ad972c1c7e465c46e87b8ec1
김재령 <wofud0321@naver.com>
2023-09-16 23:56:41 +0900

    userdto of 생성

--- src/main/java/com/example/JBoard/Dto/UserDto.java
+++ src/main/java/com/example/JBoard/Dto/UserDto.java
@@ -3,13 +3,17 @@ package com.example.JBoard.Dto;
 import com.example.JBoard.Entity.UserAccount;
 
 public record UserDto(
-        String userId,
+        String uid,
         String password,
         String username,
         String email,
         String nickname
 ) {
+
+    public static UserDto of(String uid, String password, String username, String email, String nickname) {
+        return new UserDto(uid, password, username, email, nickname);
+    }
     public UserAccount toEntity() {
-        return UserAccount.of(userId, password, username, email, nickname);
+        return UserAccount.of(uid, password, username, email, nickname);
     }
 }
