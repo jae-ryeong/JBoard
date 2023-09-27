14d45138f6858ea85f4ad3645127d3d1b0058e56
김재령 <wofud0321@naver.com>
2023-09-27 15:27:14 +0900

    auditing

--- src/main/java/com/example/JBoard/Entity/AuditingFields.java
+++ src/main/java/com/example/JBoard/Entity/AuditingFields.java
@@ -1,13 +1,19 @@
 package com.example.JBoard.Entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrePersist;
 import lombok.Getter;
+import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Getter
 @EntityListeners(AuditingEntityListener.class)
@@ -17,4 +23,13 @@ public abstract class AuditingFields {
     @CreatedDate
     @Column(updatable = false)
     private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedAt;
+
+    @CreatedBy
+    private String creatdBy;
+
+
+
 }
