b17b89dbe2313a1aee274f7ce66bec018303f56f
김재령 <wofud0321@naver.com>
2023-10-06 18:45:54 +0900

    댓글 저장 서비스 구현

---
+++ src/main/java/com/example/JBoard/controller/CommentController.java
@@ -0,0 +1,28 @@
+package com.example.JBoard.controller;
+
+import com.example.JBoard.Dto.BoardPrincipal;
+import com.example.JBoard.Dto.Response.ArticleCommentRequest;
+import com.example.JBoard.service.ArticleCommentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/comments")
+public class CommentController {
+
+    private final ArticleCommentService commentService;
+
+    @PostMapping("/new")
+    public String newComment(ArticleCommentRequest articleCommentRequest, @AuthenticationPrincipal BoardPrincipal boardPrincipal) { // TODO: select 후 update 쿼리가 날라간다 수정해야함
+        System.out.println("articleCommentRequest = " + articleCommentRequest);
+        System.out.println("댓글 생성");
+
+        commentService.saveArticleComment(articleCommentRequest.toDto(boardPrincipal.toDto()));
+
+        return "redirect:/detail/" + articleCommentRequest.articleId();
+    }
+}
--- src/main/java/com/example/JBoard/service/ArticleCommentService.java
+++ src/main/java/com/example/JBoard/service/ArticleCommentService.java
@@ -1,8 +1,29 @@
 package com.example.JBoard.service;
 
+import com.example.JBoard.Dto.ArticleCommentDtoC;
+import com.example.JBoard.Entity.Article;
+import com.example.JBoard.Entity.UserAccount;
+import com.example.JBoard.Repository.ArticleCommentRepository;
+import com.example.JBoard.Repository.ArticleRepository;
+import com.example.JBoard.Repository.UserAccountRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
+@Transactional
+@RequiredArgsConstructor
 public class ArticleCommentService {
 
+    private final ArticleRepository articleRepository;
+    private final ArticleCommentRepository articleCommentRepository;
+    private final UserAccountRepository userAccountRepository;
+
+    public void saveArticleComment(ArticleCommentDtoC dto) {
+        Article article = articleRepository.getReferenceById(dto.getArticleId());
+        Optional<UserAccount> userAccount = userAccountRepository.findByUid(dto.getUserAccountDto().uid());
+        articleCommentRepository.save(dto.toEntity(article, userAccount.get()));
+    }
 }
