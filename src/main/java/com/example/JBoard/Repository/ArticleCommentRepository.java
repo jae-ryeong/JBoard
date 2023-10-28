fb02daffa4f30217f04d92fa0b4285c8033f138d
김재령 <wofud0321@naver.com>
2023-10-28 10:14:36 +0900

    답글이 아닌 댓글만을 위한 쿼리 추가

--- src/main/java/com/example/JBoard/Repository/ArticleCommentRepository.java
+++ src/main/java/com/example/JBoard/Repository/ArticleCommentRepository.java
@@ -1,13 +1,22 @@
 package com.example.JBoard.Repository;
 
+import com.example.JBoard.Entity.Article;
 import com.example.JBoard.Entity.ArticleComment;
 import com.example.JBoard.Entity.UserAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
     List<ArticleComment> findAllByArticleArticleId(Long articleId);
 
+    List<ArticleComment> findAllByArticleArticleIdAndParentIsNull(Long articleId);
+
     UserAccount findByUserAccountId(Long userId);
+
+    List<ArticleComment> findByArticleOrderByParentOrderAsc(Article article);
+
+    List<ArticleComment> findByParentOrderIs
 }
