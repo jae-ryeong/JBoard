fa43ae8692696e81aec46ea9f58ec64f39fb3664
김재령 <wofud0321@naver.com>
2023-11-12 10:03:50 +0900

    MD5 -> uuid로 바꿔주었다

--- src/main/java/com/example/JBoard/controller/BoardController.java
+++ src/main/java/com/example/JBoard/controller/BoardController.java
@@ -4,27 +4,35 @@ import com.example.JBoard.Dto.*;
 import com.example.JBoard.Dto.Response.ArticleResponse;
 import com.example.JBoard.Entity.UserAccount;
 import com.example.JBoard.service.*;
-import com.example.JBoard.util.MD5Generator;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
-import java.security.NoSuchAlgorithmException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
+import java.util.UUID;
 
 @RequiredArgsConstructor
 @Controller
@@ -67,14 +75,14 @@ public class BoardController {
         // 스프링 시큐리티에서 필터로 걸러주기 때문에 따로 처리 X
         return "articles/boardCreateForm";
     }
-
     @PostMapping("/boardCreateForm")
     public String CreateForm(ArticleDtoC articleDtoC, @AuthenticationPrincipal BoardPrincipal boardPrincipal, @RequestParam("file")MultipartFile files) {
-        UserAccount user = userService.getUser(boardPrincipal.getUsername());
-
         try{
             String origFilename = files.getOriginalFilename();
-            String filename = new MD5Generator(origFilename).toString();
+
+            String uuid = UUID.randomUUID().toString();
+            String extension = origFilename.substring(origFilename.lastIndexOf("."));
+            String savedName = uuid + extension;
 
             // 실행되는 위치의 'files' 폴더에 파일이 저장
             String savePath = System.getProperty("user.dir") + "\\files";
@@ -88,17 +96,18 @@ public class BoardController {
                 }
             }
 
-            String filePath = savePath + "\\" + filename;
+            //String filePath = savePath + "\\" + filename;
+            String filePath = savePath + "\\" + savedName;
+            System.out.println("filePath = " + filePath);
             files.transferTo(new File(filePath));
 
-            FileDto fileDto = new FileDto(null, origFilename, filename, filePath);
+            FileDto fileDto = new FileDto(null, origFilename, savedName, filePath);
 
             Long fileId = fileService.saveFiles(fileDto);
 
+            UserAccount user = userService.getUser(boardPrincipal.getUsername());
             articleService.createArticle(articleDtoC, user, fileId);
         } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        } catch (NoSuchAlgorithmException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
@@ -110,12 +119,15 @@ public class BoardController {
     @GetMapping("/detail/{articleId}")
     public String article_detail(@PathVariable("articleId") Long articleId, Model model, @AuthenticationPrincipal BoardPrincipal boardPrincipal, HttpServletRequest request, HttpServletResponse response) {
         articleService.readArticle(articleId, request, response);
-        model.addAttribute("article", ArticleResponse.from(articleService.getArticle(articleId)));
+        ArticleDtoC article = articleService.getArticle(articleId);
+        model.addAttribute("article", ArticleResponse.from(article));
 
         List<ArticleCommentDtoC> articleComments = commentService.getArticleComments(articleId);
+        String fileName = fileService.getFile(article.getFileId()).orgNm();
 
         model.addAttribute("comments", articleComments);
         model.addAttribute("boardPrincipal", boardPrincipal);
+        model.addAttribute("filename", fileName);
         return "articles/detail";
     }
 
@@ -147,4 +159,15 @@ public class BoardController {
         return "redirect:/detail/" + articleId;
     }
 
+    @GetMapping("/download/{fileId}")
+    public ResponseEntity<Resource> fileDownload(@PathVariable("fileId") Long fileId) throws IOException {
+        FileDto fileDto = fileService.getFile(fileId);
+        Path path = Paths.get(fileDto.savedPath());
+        Resource resource = new InputStreamResource(Files.newInputStream(path));
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType("application/octet-stream"))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + fileDto.orgNm() + "\"")
+                .body(resource);
+    }
 }
