cbfbf61ff0031ae91fc79a1bb4cab964ed0b8424
김재령 <wofud0321@naver.com>
2024-04-08 22:57:52 +0900

    사용하지 않는 클래스 제거

--- src/main/java/com/example/JBoard/exception/FileNotFountException.java
+++
@@ -1,8 +0,0 @@
-package com.example.JBoard.exception;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(HttpStatus.NOT_FOUND)
-public class FileNotFountException extends RuntimeException{
-}
